73cdb7b03ed7ebd071eca7ed96ad0f7ade348ada
gadereshma5 <gadereshma5@gmail.com>
2018-04-23 17:56:12 +0530

    23rd April 2018

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from classes.game import person,bcolors
 
-player=
+magic=[{"name":"fire","cost":10,"dmg":60},
+{"name":"thunder","cost":10,"dmg":60},
+{"name":"dlizzard","cost":10,"dmg":60}]
+
+player=person(460,65,60,34,magic)
+print(player.generate_damage())
+print(player.generate_damage())
+print(player.generate_damage())
